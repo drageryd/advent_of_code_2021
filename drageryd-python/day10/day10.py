d8c19bfeb0a0418c45d762ba2bd6b7be1fabda7e
drageryd <4681360+drageryd@users.noreply.github.com>
2021-12-10 21:48:42 +0100

    Day 10

--- drageryd-python/day10/day10.py
+++ drageryd-python/day10/day10.py
@@ -1,143 +1,62 @@
 import  sys
 
+# Parse input
 data = sys.stdin.read().strip()
-sdata = """[({(<(())[]>[[{[]{<()<>>
-[(()[<>])]({[<{<<[]>>(
-{([(<{}[<>[]}>{[]{[(<()>
-(((({<>}<{<{<>}{[]{[]{}
-[[<[([]))<([[{}[[()]]]
-[{[{({}]{}}([{[{{{}}([]
-{<[[]]>}<{[{[{[]{()[[[]
-[<(<(<(<{}))><([]([]()
-<{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]"""
+lines = [list(x) for x in data.split("\n")]
 
-lines = data.split("\n")
-#heightmap = {x+y*1j:int(v) for y,row in enumerate(data.split("\n")) for x,v in enumerate(row)}
+# Matching brackets and score system
+matching = {"[": "]", "(": ")", "{": "}", "<": ">"}
+scores = {")": (3,1), "]": (57,2), "}": (1197,3), ">": (25137,4)}
 
-m = {
-    "[": "]",
-    "(": ")",
-    "{": "}",
-    "<": ">",
-}
-
-scores = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137,
-}
-
+# Recursively parse every line
 def parse(line):
-    #print(line, len(line))
-    if len(line) < 2:
-        #print("invalid")
-        if line and line[0] in scores:
-            return 0, scores[line[0]], ""
-        else:
-            #print("incomplete")
-            return 0, 0, line[0]
-    # Get opening
-    o = line[0]
-    #print(o)
-    # Peek at next 
-    if line[1] == m[o]:
-        # Bracket closing
-        #print(line[1])
-        #print("valid")
-        return 2, 0, ""
+    # Pop opening bracket
+    opening = line.pop(0)
+    # Loop as long as the next character is an opening bracket
+    # If some recursive call returned incomplete, break
+    ret = ""
+    while not ret and line and line[0] in matching:
+        ret = parse(line)
+    # If there is no more characters, the line is incomplete
+    if not line:
+        return opening + ret
+    # Else check if the closing bracket is invalid
+    closing = line.pop(0)
+    if closing != matching[opening]:
+        line.clear()
+        return closing
+    # Reaching end if valid
+    return ""
+
+# Parse input once and collect invalid and incomplete lines
+invalid = []
+incomplete = []
+for line in lines:
+    # Some lines have a valid start
+    # Parse them until whole line is consumed
+    while line:
+        res = parse(line)
+    # res is empty if ok
+    if not res:
+        continue
+    # Either invalid or incomplete
+    # If the last read char is closing it is invalid
+    if res[-1] in scores:
+        invalid.append(res[-1])
     else:
-        y = 1
-        x, s, l = parse(line[y:])
-        while x != 0:
-            y += x
-            #print(y)
-            if y >= len(line):
-                #print("incomplete2", o + l)
-                return y + 1, 0, o + l
-            #print(y)
-            if line[y] == m[o]:
-                #print(line[y])
-                #print("valid")
-                return y + 1, 0, l
-            elif line[y] in m:
-                x, s, l = parse(line[y:])
-            elif line[y] in scores:
-                #print("Expected {}, but found {} instead".format(m[o], line[y]))
-                return 0, scores[line[y]], line[y:]
-            else:
-                #print("invalid")
-                return 0, 0, o + l
-        #print(s)
-        #if not l:
-        #    print(l)
-        return 0, s, o + l
+        incomplete.append(res)
 
-#parse("[]")
-#parse("([])")
-#parse("{()()()}")
-#parse("<([{}])>")
-#parse("[<>({}){}[([])<>]]")
-#parse("(((((((((())))))))))")
+# Part 1 - Invalid lines
+print("Part 1: {}".format(sum(scores[i][0] for i in invalid)))
 
-#print(parse("(]"))
-#print(parse("{()()()>"))
-#print(parse("(((()))}"))
-#print(parse("<([]){()}[{}])"))
+# Part 2 - Incomplete lines
+def calculate_score(incomplete):
+    score = 0
+    while incomplete:
+        score *= 5
+        score += scores[matching[incomplete.pop()]][1]
+    return score
 
-#print(parse("{[([{[[[{({<<[<>[]]<()[]>>(<[]<>>({}))><{[{}()]{<>{}}}<<{}{}>{{}[]}>>}<<{{<>()}<()()>}<[(){}]<[]{"))
-
-# Part 1
-"""
-score = 0
-for line in lines:
-    o, s, l = parse(line)
-    score += s
-print(score)
-"""
-
-
-#print(parse("[({(<(())[]>[[{[]{<()<>>"))
-
-def p2(line):
-    o, s, l = parse(line)
-    if s:
-        return ""
-    #print(o, s, l)
-    #print(o, s, l)
-    while o and o < len(line):
-        line = line[o:]
-        #print(line)
-        o, s, l = parse(line)
-    return l
-#print(parse("(((({<>}<{<{<>}{[]{[]{}"))
-#print(parse("{<[[]]>}<{[{[{[]{()[[[]"))
-#print(parse("<{([{{}}[<[[[<>{}]]]>[]]"))
-
-scores = {
-    ")": 1,
-    "]": 2,
-    "}": 3,
-    ">": 4,
-}
-
-# Part 2
-score = []
-for line in lines:
-    inc = p2(line)
-    if inc:
-        #print("".join(inc))
-        inc = list(inc)
-        s = 0
-        while inc:
-            c = inc.pop()
-            #print(c, s)
-            s *= 5
-            s += scores.get(m.get(c, 0), 0)
-        #print(s)
-        score.append(s)
-
-score = sorted(score)
-
-print(score[(len(score) - 1) // 2])
+incomplete_scores = [calculate_score(list(i)) for i in incomplete]
+incomplete_scores.sort()
+print("Part 2: {}".format(incomplete_scores[(len(incomplete_scores) - 1) // 2]))
